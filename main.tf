353cbd9f2b5f45f33b12037b55a85e256fc2baba
vsreeni <vsreenivas459@gmail.com>
2018-08-21 19:48:14 -0400

    Initialize tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 provider "aws" {
-  profile    = "AWS-Sandbox"
+  profile    = "vs-terraform"
   region     = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tf-sre-pled-com"
+    profile    = "vs-terraform"
+    key    = "s3/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
